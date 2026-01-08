23079a4d8a9a8f74d20f2be939183be970230fd8
Siddhant Deshmukh <112260375+siddhant-2002@users.noreply.github.com>
2026-01-08 23:12:06 +0530

    Time: 4 ms (54.92%) | Memory: 10 MB (45.35%) - LeetSync

--- leetcode/567-permutation-in-string/permutation-in-string.cpp
+++ leetcode/567-permutation-in-string/permutation-in-string.cpp
@@ -1,46 +1,31 @@
 class Solution {
-    bool allzero(map<char,int>mp){
-        for (auto it : mp){
-            if(it.second != 0){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool helper(map<char,int> mp, string s2, int left, int right){
-        for(int i=left;i<right;i++){
-            if(mp.find(s2[i])!=mp.end()){
-                mp[s2[i]]--;
-            }
-        }
-        return allzero(mp);
-    }
 public:
     bool checkInclusion(string s1, string s2) {
-        map<char,int> mp;
         if(s1.size()>s2.size()){
             return false;
         }
-        for(auto it:s1){
-            mp[it]++;
-        }
-        int left=0;
-        int right=s1.size();
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
 
-       bool ans = helper(mp,s2,left,right);
+        for(int i=0;i<s1.size();i++){
+            v1[s1[i]-'a']++;
+        }
 
-        for(int i=right;i<s2.size();i++){
-            if(ans==true){
-                return ans;
+        for(int i=0;i<s1.size();i++){
+            v2[s2[i]-'a']++;
+        }
+         if(v1==v2){
+                return true;
             }
-            else{
-                left+=1;
-                right+=1;
-                ans = helper(mp,s2,left,right);
+
+        for(int i = s1.size(); i < s2.size();i++){
+           
+            v2[s2[i-s1.size()]-'a']--;
+            v2[s2[i]-'a']++;
+             if(v1==v2){
+                return true;
             }
         }
-        return ans;
-        
+        return false;
     }
 };
